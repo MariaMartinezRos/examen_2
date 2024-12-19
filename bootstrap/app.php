8d1a6fb15c884935773a6f09739d011c69f05d67
MariaMartinezRos <cibernuggets@gmail.com>
2024-12-19 15:26:51 +0100

    Ejercicio 1: Versión 3 de la API - FINALIZADO

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
             Route::middleware('api')
                 ->prefix('api/v2')
                 ->group(base_path('routes/api_v2.php'));
+            Route::prefix('api/v3')->group(function () {
+                require __DIR__.'/../routes/api_v3.php';
+            });
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
