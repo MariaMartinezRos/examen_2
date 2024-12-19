16ce97c3281e4a5d757d10932a7e3405f14a7440
MariaMartinezRos <cibernuggets@gmail.com>
2024-12-19 16:39:45 +0100

    ej 7 - FINALIZADO

--- routes/api_v3.php
+++ routes/api_v3.php
@@ -12,9 +12,13 @@ Route::get('/user', function (Request $request) {
 Route::get('lists/categories', [CategoryController::class,  'list']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('categories', CategoryController::class);
-    Route::get('products', [ProductController::class,  'index'])
-        ->middleware('throttle:products');
+    Route::apiResource('products', ProductController::class);
+//    Route::get('products', [ProductController::class,  'index'])
+//        ->middleware('throttle:products');
     Route::get('/private-subcategories', [SubcategoryController::class, 'privateIndex']);
 });
 
+
 Route::get('/subcategories', [SubcategoryController::class, 'index']);
+
+
