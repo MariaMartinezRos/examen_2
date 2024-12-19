48cd165dc37478ddd7241503d0b7483068a1527d
MariaMartinezRos <cibernuggets@gmail.com>
2024-12-19 15:37:15 +0100

    Ejercicio 2: Añadir una tabla Subcategorías - A MEDIAS

---
+++ app/Models/Subcategory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Subcategory extends Model
+{
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
+}
